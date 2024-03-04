24b77cd52e691c359610d0248ecd9c401a31af0c
cinar15 <125133778+cinar15@users.noreply.github.com>
2024-03-04 22:39:34 +0300

    Add files via upload

---
+++ odev_donusturme/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using odev_donusturme.Models;
+
+namespace odev_donusturme.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    
+    public IActionResult Gezi()
+    {
+        return View();
+    }
+
+
+    public IActionResult Anasayfa()
+    {
+        return View();
+    }
+
+        public IActionResult Kategori()
+    {
+        return View();
+    }
+
+            public IActionResult Hakkimizda()
+    {
+        return View();
+    }
+
+
+            public IActionResult İletisim()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ odev_donusturme/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace odev_donusturme.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
